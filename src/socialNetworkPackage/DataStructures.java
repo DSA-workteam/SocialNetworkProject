b6f7aeb2b2cfef7bf9f48a0fcbb174fc6aa4b563
Borja <bjmoralejo67@gmail.com>
2020-09-19 11:30:40 +0200

    Adding project into Github
    For better teamworking we will use Github. Like that we can have our report and workplan logged at the same time.

---
+++ src/socialNetworkPackage/DataStructures.java
@@ -0,0 +1,21 @@
+package socialNetworkPackage;
+
+import java.util.ArrayList;
+
+public class DataStructures {
+
+	public class Person {
+		public String id;
+		public String name;
+		public ArrayList<String> surnames;
+		public String birthdate;
+		public String birthplace;
+		public String home;
+		public ArrayList<String> studiedat;
+		public ArrayList<String> workedat;
+		public ArrayList<String> movies;
+		public String groupcode;
+	}
+	
+	
+}
