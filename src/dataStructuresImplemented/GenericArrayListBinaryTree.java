b8c24105e1a32edfca2ac8b64f2fe2c54de305e7
Borja <bjmoralejo67@gmail.com>
2020-09-26 20:02:34 +0200

    Generic implementation of 2 ADT
    ArrayList BinaryTree
    ArrayList HashMap

--- src/dataStructuresImplemented/GenericArrayListBinaryTree.java
+++ src/dataStructuresImplemented/GenericArrayListBinaryTree.java
@@ -1,43 +1,101 @@
 package dataStructuresImplemented;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import adt.BinaryTreeADT;
 
-public class GenericArrayListBinaryTree<T, K> implements BinaryTreeADT<T, K>{
+public class GenericArrayListBinaryTree< T, K extends Comparable<K> >  implements BinaryTreeADT<T, K>{
 
-	@Override
-	public Collection<T> getElemets(K searchKey) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private GenericArrayListBinaryTree<T, K> less,more;
+	private K key;
+	private ArrayList<T> elements;
+	private int N;
+	
+
+	public GenericArrayListBinaryTree(){
+		elements = new ArrayList<T>();
+		N = 0;
+	}
+	public GenericArrayListBinaryTree(T element, K key){
+		super();
+		this.key = key;
+		elements.add(element);
 	}
 
 	@Override
-	public void addElement(T element) {
-		// TODO Auto-generated method stub
+	public Collection<T> getElemets(K searchKey) {
+		int comparation = key.compareTo(searchKey);
+		Collection<T> ret = null;
+		if(comparation == 0) {
+			ret = elements;			
+		}else if(comparation > 0) {
+			ret = less.getElemets(searchKey);
+		}else {
+			ret = more.getElemets(searchKey);
+		}
+		return ret;
+	}
+
+	@Override
+	public void addElement(T element, K addKey) {
+		int comparation = key.compareTo(addKey);
+		if(comparation == 0) {
+			elements.add(element);
+		}else if(comparation > 0) {
+			if(less != null)
+				less.addElement(element,addKey);
+			else less = new GenericArrayListBinaryTree<T, K>();
+		}else {
+			if(more != null)
+				more.addElement(element, addKey);
+			else more = new GenericArrayListBinaryTree<T, K>();
+		}
 		
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return N;
 	}
 
+	public int allTreeSize() {
+		int ret = N;
+		if(less != null)
+			ret += less.allTreeSize();
+		if(more != null)
+			ret += more.allTreeSize();
+		return ret;
+	}
+	
+	
 	@Override
 	public int depth() {
-		// TODO Auto-generated method stub
-		return 0;
+		int dL = 0,dM = 0;
+		if(less != null )
+			dL = less.depth();
+		if(more != null)
+			dM = more.depth();
+		
+		return Math.max(dL, dM) + 1;
+	 	
 	}
+
 
 	@Override
-	public void removeElement(T element) {
-		// TODO Auto-generated method stub
+	public void removeElement(T element, K remKey) {
+		int comparation = key.compareTo(remKey);
+		if(comparation == 0) {
+			elements.remove(element);
+		}else if(comparation > 0) {
+			if(less != null)
+				less.removeElement(element,remKey);			
+		}else {
+			if(more != null)
+				more.removeElement(element, remKey);
+		}
 		
 	}
 
-	
-	public GenericArrayListBinaryTree(){
-		
-	}
 }
---
+++ src/dataStructuresImplemented/GenericArrayListHashMap.java
@@ -0,0 +1,63 @@
+package dataStructuresImplemented;
+
+import java.util.ArrayList;
+
+import adt.HashMapADT;
+
+public class GenericArrayListHashMap<T,K> implements HashMapADT<T, K> {
+
+	private ArrayList<T>[] hashMap;
+	private final static int DEFAULT_SIZE = 128, DEFAULT_ARRAYLIST_SIZE = 16;
+	private int mapSize = 0;
+	private int N;
+	public GenericArrayListHashMap() {
+		
+		this(DEFAULT_SIZE);
+	
+	}
+	
+	public GenericArrayListHashMap(int initial_size_map) {
+		this(initial_size_map,DEFAULT_ARRAYLIST_SIZE);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public GenericArrayListHashMap(int initial_size_map, int initial_size_ArrayList) {
+		hashMap =  (ArrayList<T>[])(new Object[initial_size_map]);
+		mapSize = initial_size_map;
+		N = 0;
+		for(int i = 0; i < initial_size_map;i++) {
+			hashMap[i] = new ArrayList<T>(initial_size_ArrayList);
+		}
+	}
+	
+	@Override
+	public void put(K key, T element) {		
+		
+		if(!isIn(key, element)) {
+			hashMap[key.hashCode()% mapSize].add(element);
+			N++;
+		}
+		
+	}
+	@Override
+	public boolean isIn(K key, T element) {
+		return hashMap[key.hashCode()% mapSize].contains(element);
+	}
+	
+	@Override
+	public boolean remove(K key, T element) {
+			return hashMap[key.hashCode()% mapSize].remove(element);
+	}
+	
+	@Override
+	public ArrayList<T> get(K key) {
+		return hashMap[key.hashCode() % mapSize];
+	}
+
+	@Override
+	public int size() {
+		
+		return N;
+	}
+
+}
