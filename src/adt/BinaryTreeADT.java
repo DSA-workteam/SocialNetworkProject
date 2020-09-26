5d9135552bbc820e99551c0df2c8830081cd9519
Borja <bjmoralejo67@gmail.com>
2020-09-26 19:41:06 +0200

    BinaryTreeADT interface explanation
    This interface is an abstract data structure which we can use to find data neighbors easily. For example, if we are looking for people that likes pizza, we first search(*) the binary tree which contains the data pizza and with that all related people are inside a collection.

--- src/adt/BinaryTreeADT.java
+++ src/adt/BinaryTreeADT.java
@@ -2,16 +2,20 @@ package adt;
 
 import java.util.Collection;
 
-public interface BinaryTreeADT<T,K> {
+
+
+public interface BinaryTreeADT< T, K extends Comparable<K> >  {
 
 	public Collection<T> getElemets(K searchKey);
 	
-	public void addElement(T element);
+	public void addElement(T element, K addKey);
 	
 	public int size();
 	
+	public int allTreeSize();
+	
 	public int depth();
 	
-	public void removeElement(T element);
+	public void removeElement(T element,K remKey);
 	
 }
