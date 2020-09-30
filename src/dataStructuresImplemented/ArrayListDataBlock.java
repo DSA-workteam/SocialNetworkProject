3b8ac56e5db7b492c8d7d5c2cf05bea64e32dbed
Borja <bjmoralejo67@gmail.com>
2020-09-30 11:32:14 +0200

    Person class datastructure and implementing ArrayListDataBlock
    Person datastructure is an 2D array of StringDataBlocks.

--- src/dataStructuresImplemented/ArrayListDataBlock.java
+++ src/dataStructuresImplemented/ArrayListDataBlock.java
@@ -1,33 +1,54 @@
 package dataStructuresImplemented;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import adt.DataBlockADT;
 
-public class ArrayListDataBlock implements DataBlockADT{
+public class ArrayListDataBlock<T,K extends Comparable<K>> implements DataBlockADT<T,K>{
 
+	
+	private K key;
+	private ArrayList<T> list;
+	private int N;
+	
+	
+	public ArrayListDataBlock() {
+		N = 0;
+		list = new ArrayList<T>();
+	}
+	
+	
 	@Override
-	public Comparable getKey() {
-		// TODO Auto-generated method stub
-		return null;
+	public K getKey() {
+		return key;
 	}
 
 	@Override
-	public Collection getCollection() {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<T> getCollection() {
+		return list;
 	}
 
 	@Override
-	public void add(Object element) {
-		// TODO Auto-generated method stub
+	public void add(T element) {
+		if(!list.contains(element)) {
+			list.add(element);
+			N++;
+		}
 		
 	}
 
 	@Override
-	public void remove(Object element) {
-		// TODO Auto-generated method stub
+	public void remove(T element) {
+		if(list.remove(element))
+			N--;
 		
+	}
+
+
+	@Override
+	public int size() {		
+		return N;
 	}
 
 }
--- src/dataStructuresImplemented/Person.java
+++ src/dataStructuresImplemented/Person.java
@@ -5,17 +5,30 @@ import java.lang.ref.Reference;
 public class Person {
 	
 	// Attributes
+	public class StringDataBlock extends ArrayListDataBlock<String,String>{
+		public StringDataBlock() {
+			
+		}
+	}
 	
-	private String id;
+	private ArrayListDataBlock<String, String>[][] attributes;
 	
-	private String name;
-	private String username;
-	private String birthdat;
-	private String birthplace;
-	private String home;
-	private String studiedat;
-	private String workedat;
-	private String movies;
-	private String groupcode;
+	public static final int N_PARAMETERS = 10;
+	
+	
+	public Person(String combinedData) {
+		
+		attributes = new StringDataBlock[N_PARAMETERS][];
+		String[] separatedData = combinedData.split(",");
+		for(int i =1;i < separatedData.length;i++) {
+			String[] attributesData = separatedData[i].split(";");
+			attributes[i] = new StringDataBlock[attributesData.length];
+			for(int j = 0; j < attributesData.length;j++) {
+				attributes[i][j] = null;
+
+			}
+			
+		}
+	}
 	
 }
