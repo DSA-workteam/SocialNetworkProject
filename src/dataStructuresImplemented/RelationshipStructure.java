9a93e3bd4ce7b1f9777c73f5903ac13781a8b982
WicruX <bjmoralejo67@gmail.com>
2020-10-05 09:50:50 +0200

    Tutorial
    Para imoke.

---
+++ src/dataStructuresImplemented/RelationshipStructure.java
@@ -0,0 +1,12 @@
+package dataStructuresImplemented;
+
+import adt.NodeADT;
+
+public class RelationshipStructure {
+	
+	private NodeADT<String> nodos;
+	public RelationshipStructure() {
+		nodos = new GenericArrayListNode<String>();
+	}
+	
+}
