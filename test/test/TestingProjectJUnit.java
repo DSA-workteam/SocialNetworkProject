3c5e171020adf0a0a666926ca6fa3c62724ceb84
WicruX <bjmoralejo67@gmail.com>
2020-10-15 16:32:36 +0200

    Creating JUnit test plan
    Adding empty Junit cases

---
+++ test/test/TestingProjectJUnit.java
@@ -0,0 +1,120 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.ImpulsoryAttributeRequiredException;
+
+class TestingProjectJUnit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	private class PersonTester{
+		
+		
+	}
+	
+	
+	private class FileManagement{
+		
+		void load() {
+			
+		}
+		
+		void printIntoFile() {
+			
+		}
+		
+		void checkIfSame() {
+			
+		}
+		
+		
+	}
+	
+	private class DataHolderLimitTesting{
+		
+		void create() {
+			
+		}
+		
+		void fillDataHolder() {
+			
+		}
+		
+		void emptyDataHolder() {
+			
+		}
+		
+	}
+	
+	private class TestingPeopleLoader {
+		
+		void createEmptyDataHolder() {
+			
+		}
+		
+		void addPersonWithAllAttributes() {
+			
+		}
+		
+		void addPersonWithoutId() throws ImpulsoryAttributeRequiredException {
+			
+		}
+		
+		void multipleInputsInUniqueAttributes() {
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+	private class RelationshipsTests{
+		
+		void createRelationships() {
+			
+		}
+		
+		void removeAllRelationships() {
+			
+		}
+		
+		
+		void removeAPersonFromNetwork() {
+			
+		}
+		
+	}
+	
+	
+	private class DataBlockTesting{
+	
+		void createDatablock() {
+			
+		}
+		
+		void addIntoDataBlock() {
+			
+		}
+		
+		void removeFromDataBlock() {
+			
+		}
+		
+		void checkRelatedInDataBlock() {
+			
+		}
+		
+	}
+	
+
+}
