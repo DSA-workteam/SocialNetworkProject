951b3f6b1676d52dba118c31bc2d9b119fc45bbf
ImanolMaraña <imamar2001@gmail.com>
2020-11-12 18:36:01 +0100

    Binary tree implementation

--- src/abstractDataTypesImplemented/GenericArrayListBinaryTree.java
+++ src/abstractDataTypesImplemented/GenericArrayListBinaryTree.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import abstractDataTypesPackage.BinaryTreeADT;
+import exceptions.ElementNotFoundException;
 /**
  * Implementation made with ArrayList of BinaryTreeADT.
- * @author Borja Moralejo Tobajas
+ * @author Borja Moralejo Tobajas & Imanol Maraña Hurtado
  *
  * @param <T> Element.
  * @param <K> Key. K type must extend ComparableK
@@ -21,9 +22,11 @@ public class GenericArrayListBinaryTree< T, K extends Comparable<K> >  implement
 	
 
 	public GenericArrayListBinaryTree(T element, K key){
-		super();
+		less = null;
+		more = null;
 		this.key = key;
 		elements.add(element);
+		N = 0;
 	}
 
 	
@@ -33,76 +36,153 @@ public class GenericArrayListBinaryTree< T, K extends Comparable<K> >  implement
 		int comparation = key.compareTo(searchKey);
 		Collection<T> ret = null;
 		if(comparation == 0) {
-			ret = elements;			
-		}else if(comparation > 0) {
-			ret = less.getElemets(searchKey);
-		}else {
-			ret = more.getElemets(searchKey);
+			ret = elements;
 		}
+		else 
+			if(less != null)
+				if(comparation > 0) {
+					ret = less.getElemets(searchKey);
+				}
+			else if (more != null)
+				if(comparation < 0)
+					ret = more.getElemets(searchKey);
+		
 		return ret;
 	}
 
 	
 	
 	@Override
-	public void addElement(T element, K addKey) {
+	public boolean addElement(T element, K addKey) {
+		boolean added = true;
+		
 		int comparation = key.compareTo(addKey);
-		if(comparation == 0) {
-			elements.add(element);
-		}else if(comparation > 0) {
+		if(comparation == 0) 
+			added = false;
+		else if(comparation > 0) {
 			if(less != null)
-				less.addElement(element,addKey);
-			else less = new GenericArrayListBinaryTree<T, K>(element, addKey);
+				added = less.addElement(element,addKey);
+			else {
+				less = new GenericArrayListBinaryTree<T, K>(element, addKey);
+				N++;
+			}
 		}else {
 			if(more != null)
-				more.addElement(element, addKey);
-			else more = new GenericArrayListBinaryTree<T, K>(element, addKey);
+				added = more.addElement(element, addKey);
+			else {
+				more = new GenericArrayListBinaryTree<T, K>(element, addKey);
+				N++;
+			}
 		}
+		return added;
 		
 	}
 
 	@Override
-	public int size() {
+	public int numberOfElements() {
 		return N;
 	}
-
+	
+	
 	@Override
-	public int allTreeSize() {
-		int ret = N;
-		if(less != null)
-			ret += less.allTreeSize();
-		if(more != null)
-			ret += more.allTreeSize();
+	public int depth(K key) {
+		int comparation = key.compareTo(key);
+		int ret = 0;
+		if(comparation == 0) {
+			ret = 1;
+		}
+		else 
+			if(less != null)
+				if(comparation > 0) {
+					ret = 1 + less.depth(key);
+				}
+			else if (more != null)
+				if(comparation < 0)
+					ret = 1 + more.depth(key);
+		
 		return ret;
 	}
-	
-	
+
+
 	@Override
-	public int depth() {
-		int dL = 0,dM = 0;
-		if(less != null )
-			dL = less.depth();
-		if(more != null)
-			dM = more.depth();
-		
-		return Math.max(dL, dM) + 1;
-	 	
+	public void removeElement(K remKey) throws ElementNotFoundException{
+		// TODO
+		int lessComparation = less.key.compareTo(remKey);
+		int moreComparation = more.key.compareTo(remKey);
+		GenericArrayListBinaryTree<T, K> aux1;
+		GenericArrayListBinaryTree<T, K> aux2;
+		if(lessComparation == 0) {
+			if (less.less == null && less.more == null)
+				less = null;
+			else if(less.less == null)
+				less = less.more;
+			else if(less.more == null)
+				less = less.less;
+			else {
+				aux1 = less.less;
+				less = less.more;
+				aux2 = less;
+				while (aux2.less != null)
+					aux2 = aux2.less;
+				aux2.less = aux1;
+			}
+		}
+		else if (moreComparation == 0) {
+			if (more.less == null && more.more == null)
+				more = null;
+			else if(more.less == null)
+				more= more.more;
+			else if(more.more == null)
+				more = more.less;
+			else {
+				aux1 = more.more;
+				more = more.less;
+				aux2 = more;
+				while (aux2.more != null)
+					aux2 = aux2.more;
+				aux2.more = aux1;
+			}
+		}
+		else {
+			if(moreComparation < 0) {
+				if(more != null)
+					more.removeElement(remKey);
+				else
+					throw new ElementNotFoundException("There's no node with such key");
+			}
+			else {
+				if(less != null)
+					less.removeElement(remKey);
+				else
+					throw new ElementNotFoundException("There's no node with such key");
+			}
+		}
 	}
 
 
+
 	@Override
-	public void removeElement(T element, K remKey) {
-		int comparation = key.compareTo(remKey);
-		if(comparation == 0) {
-			elements.remove(element);
-		}else if(comparation > 0) {
-			if(less != null)
-				less.removeElement(element,remKey);			
-		}else {
-			if(more != null)
-				more.removeElement(element, remKey);
-		}
+	public int longestDepth() {
+		int dL = 0,dM = 0;
+		if(less != null )
+			dL = less.longestDepth();
+		if(more != null)
+			dM = more.longestDepth();
 		
+		return Math.max(dL, dM) + 1;
+	}
+
+
+
+	@Override
+	public int shortestDepth() {
+		int dL = 0,dM = 0;
+		if(less != null )
+			dL = less.shortestDepth();
+		if(more != null)
+			dM = more.shortestDepth();
+		
+		return Math.min(dL, dM) + 1;
 	}
 
 }
--- src/abstractDataTypesPackage/BinaryTreeADT.java
+++ src/abstractDataTypesPackage/BinaryTreeADT.java
@@ -2,20 +2,63 @@ package abstractDataTypesPackage;
 
 import java.util.Collection;
 
+import exceptions.ElementNotFoundException;
 
 
+/**
+ * ADT of the Binary tree
+ * @author Imanol Maraña Hurtado
+ *
+ * @param <T> Element
+ * @param <K> Key. K type must extend ComparableK
+ */
 public interface BinaryTreeADT< T, K extends Comparable<K> >  {
-
+	
+	
+	/**
+	 * Given a key, returns the information located in a node that matches with the given key, if there is.
+	 * @param searchKey A key to search for the correct node
+	 * @return A collection with all the information saved in the node with the given key
+	 */
 	public Collection<T> getElemets(K searchKey);
 	
-	public void addElement(T element, K addKey);
 	
-	public int size();
+	/**
+	 * Given a T element and a K key, adds a new node with the starting T element on it in it's correct position, if there is't already in the tree.
+	 * @param element Starting element of the node of the tree
+	 * @param addKey Key with which find the correct position and then assigned to it
+	 * @return a boolean that indicates if the node was created or already existed
+	 */
+	public boolean addElement(T element, K addKey);
 	
-	public int allTreeSize();
 	
-	public int depth();
+	/**
+	 * Returns the amount of nodes that the binary tree has
+	 * @return Int with the number of existing nodes
+	 */
+	public int numberOfElements();
 	
-	public void removeElement(T element,K remKey);
+	
+	/**
+	 * Returns the node located the farest of the root that has both sons null.
+	 * @return Int with the depth of the last node
+	 */
+	public int longestDepth();
+	
+	/**
+	 * Returns the depth at which a node whose key matches the given key is located
+	 * @param key Given key to compare to that of the existing nodes
+	 * @return Int with the depth of the node whose key matches that of the given key
+	 */
+	public int depth(K key);
+	
+	
+	/**
+	 * Returns the depth of the node that has at least one null son located the closest to the root
+	 * @return First node with a null son
+	 */
+	public int shortestDepth();
+	
+	public void removeElement(K remKey) throws ElementNotFoundException;
 	
 }
